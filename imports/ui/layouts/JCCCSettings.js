f22b6450024412ca78d3a4bbaabad5f5bed8294e
saarthaks <saarthak.sarup@gmail.com>
2017-09-23 16:56:52 -0400

    change admin password

--- imports/ui/layouts/JCCCSettings.js
+++ imports/ui/layouts/JCCCSettings.js
@@ -32,6 +32,17 @@ const financeRules = {
     }
 };
 
+const passwordRules = {
+    oldPassword: {
+        identifier: 'oldPassword',
+        rules: [{ type: 'empty', prompt: "Please enter your current password" }]
+    },
+    newPassword: {
+        identifier: 'newPassword',
+        rules: [{ type: 'empty', prompt: "Please enter a new password" }]
+    }
+}
+
 var updatePoc = function(template) {
     const formElem = $('.ui.form#update-poc');
     formElem.form({ fields: contactRules, inline: true });
@@ -130,6 +141,27 @@ var initFinances = function(template) {
     }
 }
 
+var updatePassword = function(template) {
+    const formElem = $('.ui.form#update-password');
+    formElem.form({ fields: passwordRules, inline: true });
+
+    if ( formElem.form('is valid')) {
+        const data = formElem.form('get values');
+        const oldPassword = data.oldPassword;
+        const newPassword = data.newPassword;
+        Accounts.changePassword(oldPassword, newPassword);
+        Template.instance().modalHeader.set("Success!");
+        Template.instance().modalMessage.set("You have successfully changed your password.");
+        $('.ui.modal').modal({inverted: true}).modal('show');
+        Meteor.setTimeout(() => {
+            $('.ui.modal').modal('hide');
+        }, 1000);
+        formElem.form('clear');
+    } else {
+        formElem.form('valudate rules');
+    }
+}
+
 Template.JCCCSettings.onCreated( function() {
     Meteor.subscribe('jccc-settings');
     const entry = JCCCSettingsDB.findOne();
@@ -170,6 +202,11 @@ Template.JCCCSettings.events({
     'submit form#init-form' : function(e, template) {
         e.preventDefault();
         initFinances(template);
+        return false;
+    },
+    'submit form#update-password' : function(e, template) {
+        e.preventDefault();
+        updatePassword(template);
         return false;
     },
     'change [name=formStatus]' : function(e, template) {
