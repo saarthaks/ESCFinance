f6841a490b0899e31669217e55dc18f74c9f8145
Saarthak Sarup <saarthak.sarup@gmail.com>
2017-10-15 16:03:56 -0400

    Updating pg (#16)
    * enabling old pg work
    * forms send emails
    * udpated about page
    * minor rule update

--- imports/ui/layouts/PGSmall.js
+++ imports/ui/layouts/PGSmall.js
@@ -10,6 +10,9 @@ Template.PGSmallLayout.onCreated( function() {
     this.currentTab = new ReactiveVar("PGSmallRules");
     this.formError = new ReactiveVar(false);
     this.errorMessage = new ReactiveVar("");
+    this.modalHeader = new ReactiveVar("");
+    this.modalMessage = new ReactiveVar("");
+
     Session.set('pgform', initForm());
 });
 
@@ -116,7 +119,15 @@ var validateForm = function() {
         && !!formFields['emails'][0]
         && !!formFields['phones'][0]
         && !!formFields['schools'][0]
-        && !!formFields['majors'][0]) {
+        && !!formFields['class'][0]
+        && !!formFields['majors'][0]
+        && !!formFields['names'][1]
+        && !!formFields['unis'][1]
+        && !!formFields['emails'][1]
+        && !!formFields['phones'][1]
+        && !!formFields['schools'][1]
+        && !!formFields['class'][1]
+        && !!formFields['majors'][1]) {
 
             if (!!formFields['projectName']
                 && !!formFields['requestedAmount']
@@ -141,6 +152,60 @@ var validateForm = function() {
     return false;
 }
 
+var generateSummary = function(formFields) {
+    const header = "Project Name: " + formFields['projectName'] + "\n"
+        + "Requested Amount: " + formFields['requestedAmount'] + "\n"
+        + "Projected Completion: " + formFields['projectedCompletion'] + "\n";
+
+    var members = "";
+    var num = formFields['names'].length;
+    for (var i = 0; i < num; i++) {
+        members = members + "Member: " + formFields['names'][i] + ", " + formFields['emails'][i] + ", " + formFields['class'][i] + "\n";
+    }
+
+    const qa = "Project Description: " + formFields['projectDescription'] + "\n"
+        + "Necessary Materials: " + formFields['necessaryMaterials'] + "\n"
+        + "Cost Breakdown: " + formFields['costBreakdown'] + "\n"
+        + "Estimated Timeline: " + formFields['estimatedTimeline'] + "\n"
+        + "Feasibility: " + formFields['feasibility'] + "\n"
+        + "Community Benefit: " + formFields['communityBenefit'] + "\n"
+        + "Additional Info: " + formFields['additionalInfo'] + "\n";
+
+    const body = header + "\n" + members + "\n" + qa;
+
+    return body;
+}
+
+var sendEmails = function(formFields) {
+    // send email to applicant
+    var to = formFields['emails'][0];
+    const from = "ESC Finance Committee <ss4754@columbia.edu>";
+    const cc = "ss4754@columbia.edu";
+    var subject = "ESC Project Grant Application: Next Steps";
+    var body = "Hi!\n\n"
+         + "Thank you for beginning your ESC Project Grant! There are still a few parts to the application, so please make sure you’ve completed the following instructions by 11:59pm, October 27th, to be considered in time.\n\n"
+         + "Instructions: \n"
+         + "1. Visit the following links to generate your budget and presentation templates:\n"
+         + "https://docs.google.com/a/columbia.edu/spreadsheets/d/1bsLQc0V0rSFqgfxn1grM6i7FGbtC7BScnycY4HcV_Lk/copy\n"
+         + "https://docs.google.com/presentation/d/16SeU_9bi50uQrdl9JLrMWQHZOCsQpkexTO1q_g7wSUU/copy\n"
+         + "2. Share the documents with us at ss4754@columbia.edu as well as with the rest of your teammates.\n"
+         + "3. Fill in the templates! If you have any questions, feel free to reach out to us at ss4754@columbia.edu and we will help as best we can.\n"
+         + "3. Share the documents with us at treasurers@columbia.edu and with your club advisor.\n"
+         + "4. Sign up for a presentation slot here:\n"
+         + "https://docs.google.com/a/columbia.edu/spreadsheets/d/1BDy05XKuAcDtB4BnWNoQ2W1aUXt07N5GD7TeFDl0BOo/edit?usp=sharing\n\n"
+         + "Good luck, and we look forward to reviewing your application!\n\n"
+         + "Best Regards,\n"
+         + "ESC Finance\n";
+    Meteor.call('sendEmailWithCC', to, from, subject, body, cc);
+
+    // send receipt back to me
+    to = "ss4754@columbia.edu";
+    subject = "[ESC] Small Scale Project Grant Receipt";
+    body = generateSummary(formFields);
+
+    Meteor.call('sendEmail', to, from, subject, body)
+}
+
 var submitPGForm = function() {
     saveFormSession(Template.instance().currentTab.get());
     const formValidity = validateForm();
@@ -148,7 +213,26 @@ var submitPGForm = function() {
     const messages = [];
     if (formValidity === true) {
         Template.instance().errorMessage.set("");
-        //send email
+        try {
+            sendEmails(Session.get('pgform'));
+
+            Template.instance().modalHeader.set("Success!");
+            Template.instance().modalMessage.set("You should receive an email with your next steps from us soon. If you don't, please reach out to ss4754@columbia.edu.");
+            $('.ui.modal').modal({inverted: true}).modal('show');
+            Meteor.setTimeout(() => {
+                $('.ui.modal').modal('hide');
+                FlowRouter.go('/project-grant');
+            }, 5000);
+        } catch(e) {
+            console.log(e);
+
+            Template.instance().modalHeader.set("Error");
+            Template.instance().modalMessage.set("There was an error processing your application. Please reach out to ss4754@columbia.edu with your issue.");
+            $('.ui.modal').modal({inverted: true}).modal('show');
+            Meteor.setTimeout(() => {
+                $('.ui.modal').modal('hide');
+            }, 5000);
+        }
     } else if (formValidity === "AcceptError") {
         Template.instance().errorMessage.set("Looks like you missed a field in the Rules page! Please complete that page to continue.");
         return false;
@@ -190,6 +274,12 @@ Template.PGSmallLayout.helpers({
     },
     hasError: function() {
         return Template.instance().formError.get();
+    },
+    modalHeader: function() {
+        return Template.instance().modalHeader.get();
+    },
+    modalMessage: function() {
+        return Template.instance().modalMessage.get();
     },
     errorMessages: function() {
         return Template.instance().errorMessage.get();
